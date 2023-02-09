5afd57fc841f432d4888ced071704368d6fd9442
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-02-09 23:05:42 +0700

    feat(components): analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import defaultSEOConfig from "../../next-seo.config";
 import { Chakra } from "lib/components/Chakra";
@@ -10,18 +11,21 @@ import "lib/styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Chakra>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-        />
-      </Head>
-      <DefaultSeo {...defaultSEOConfig} />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Chakra>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Chakra>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+        </Head>
+        <DefaultSeo {...defaultSEOConfig} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Chakra>
+    </>
   );
 };
 
